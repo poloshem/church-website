4f28dfc0985b7c90f44273f4fe52c199e0ba8a38
Shem <poloshem@gmail.com>
2026-06-02 09:20:50 +0300

    Updated the constitiution

--- list_images.ps1
+++
@@ -1,5 +0,0 @@
-$root = "C:\Users\Ruknalabeer.com\Desktop\CHURCH WEBSITE"
-$exts = @(".jpg",".jpeg",".png",".gif",".webp",".bmp",".tiff")
-Get-ChildItem -Path $root -Directory -Recurse -Force | Where-Object { $_.Name -ieq "images" } | ForEach-Object {
-  Get-ChildItem -Path $_.FullName -File -Force | Where-Object { $exts -contains $_.Extension.ToLower() }
-} | Sort-Object LastWriteTime -Descending | Select-Object -First 20 FullName, LastWriteTime
